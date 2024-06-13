c7cec21e971c49abb4c29c795bb4dcc0b9d3508d
Yoona Oh <jyoonaoh@gmail.com>
2024-06-13 10:28:24 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Express!');
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
